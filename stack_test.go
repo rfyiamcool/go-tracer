865fec939eca0a2c96727067e94729072e561991
rfyiamcool <rfyiamcool@163.com>
2022-02-06 19:14:25 +0800

    fix: stack

--- stack_test.go
+++ stack_test.go
@@ -2,6 +2,8 @@ package tracer
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func BenchmarkCaller(b *testing.B) {
@@ -14,4 +16,12 @@ func BenchmarkCallerCache(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		GetCallerCache()
 	}
+}
+
+func TestTrimFuncname(t *testing.T) {
+	ss := trimClassFuncname("git.github.com/ocean/internal/controller.(*obj).run2")
+	assert.Equal(t, "controller.(*obj).run2", ss)
+
+	ss = trimFuncname("git.github.com/ocean/internal/controller.(*obj).run2")
+	assert.Equal(t, "(*obj).run2", ss)
 }
